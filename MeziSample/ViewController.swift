b8ac692b2223c0bc4098412611d16ab28c2aad4e
Taseen <tahseen0amin@gmail.com>
2017-03-20 15:40:54 +0530

    Initial Commit

---
+++ MeziSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MeziSample
+//
+//  Created by Tasin Zarkoob on 20/03/17.
+//  Copyright © 2017 Tasin Zarkoob. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
