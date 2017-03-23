15efc6c2eea64dae045aae523cd8e9d365135ee2
Taseen <tahseen0amin@gmail.com>
2017-03-23 20:43:13 +0530

    Final

--- MeziSample/ViewController.swift
+++ MeziSample/ViewController.swift
@@ -24,7 +24,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
         self.getAllPhotos()
         (self.view as! UIScrollView).isUserInteractionEnabled = false
     }
